42f3a62e885a0f1f157a33e2bb01714b87d40a39
Eric Guimarães <ericmguimaraes@gmail.com>
2015-10-03 11:41:36 -0400

    as123

---
+++ as123/proglang.pl
@@ -0,0 +1,11 @@
+language(smaltalk).
+language(lisp).
+language(prolog).
+
+essence(smaltalk,objects, 'message passing').
+essence(lisp,lists, 'recursive functions').
+essence(prolog,relations, 'logical inferencing').
+
+history(smalltalk, inventor('Alan Kay'), date(1980)).
+history(lisp,inventor('Jhon McCarthy'),date(1959)).
+history(prolog,inventor('Alan Colmeraur'),date(1971)).
---
+++ as123/proglang_plus.pl
@@ -0,0 +1,17 @@
+language(smaltalk).
+language(lisp).
+language(prolog).
+language(snobol).
+language(apl).
+
+essence(smaltalk,objects, 'message passing').
+essence(lisp,lists, 'recursive functions').
+essence(prolog,relations, 'logical inferencing').
+essence(snobol,pattern, 'string processing').
+essence(apl,matrix, 'matrix operations').
+
+history(smalltalk, inventor('Alan Kay'), date(1980)).
+history(lisp,inventor('Jhon McCarthy'),date(1959)).
+history(prolog,inventor('Alan Colmeraur'),date(1971)).
+history(snobol,inventor('Ralph Griswold'),date(1962)).
+history(apl,inventor('Kenneth Iverson'),date(1964)).
